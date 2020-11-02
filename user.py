ae30f0856ec905da7f3776e563a579746692c322
NataliaMikhaylova <natasha.mikhaylova17052002@mail.ru>
2020-11-02 16:58:30 +0300

    Задание 1

---
+++ user.py
@@ -0,0 +1,11 @@
+# его имя (например, "What is your name?")
+# возраст ("How old are you?")
+# место жительства ("Where are you live?")
+
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+home = input("Where are you live? ")
+print("This is " + name)
+print("Is " + age)
+print("(S)he live in "+ home)
